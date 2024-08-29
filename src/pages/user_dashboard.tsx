ba0fa535cacd5c34a77e268ec126ef560ac58b75
Dashrath Patel <108078023+ULTRAPAIN@users.noreply.github.com>
2024-08-30 02:53:22 +0530

    Updated the Dashboard

--- src/pages/user_dashboard.tsx
+++ src/pages/user_dashboard.tsx
@@ -1,9 +1,29 @@
-import UserLayout from "./user_layout"
-export default function Dashboard(){
-  return <>
-  <div className="p-5">
-    <h1 className="pl-20 text-4xl font-black">Dashboard</h1>
-  </div>
-  <UserLayout />
-  </>
+import UserLayout from "./user_layout";
+import { UserCardNotification } from "./user_notification";
+import UserAppbar from "./user_appbar";
+import { UserListingTable } from "./user_listingtable";
+export default function Dashboard() {
+  return (
+    <div>
+      <div>
+        <UserAppbar />
+      </div>
+    <div className="px-10 pb-10 pt-5">
+      <div>
+        <h1 className="text-2xl font-black">Dashboard</h1>
+      </div>
+      <div>
+        <UserLayout />
+      </div>
+      <div className="grid grid-cols-4 gap-4 p-5">
+        <div className="col-span-3">
+          <UserListingTable />
+        </div>
+        <div>
+          <UserCardNotification />
+        </div>
+      </div>
+    </div>
+    </div>
+  );
 }
