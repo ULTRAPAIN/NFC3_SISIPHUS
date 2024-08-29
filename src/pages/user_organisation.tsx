a3af7a9e28c2ebe9c137705f1324673a40b27497
Dashrath Patel <108078023+ULTRAPAIN@users.noreply.github.com>
2024-08-29 18:52:47 +0530

    Added the organisation and transaction page

---
+++ src/pages/user_organisation.tsx
@@ -0,0 +1,9 @@
+import UserLayout from "./user_layout"
+export default function UserOrganisation(){
+    return <div>
+  <div className="p-5">
+    <h1 className="pl-20 text-4xl font-black">Organisations</h1>
+  </div>
+  <UserLayout />
+    </div>
+}
---
+++ src/pages/user_transactions.tsx
@@ -0,0 +1,9 @@
+import UserLayout from "./user_layout"
+export default function UserTransactions(){
+    return <div>
+  <div className="p-5">
+    <h1 className="pl-20 text-4xl font-black">Transactions</h1>
+  </div>
+  <UserLayout />
+    </div>
+}
