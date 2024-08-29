b4d413ada0a9e3bd0e1c2103b60dc997fdc926a7
Dashrath Patel <108078023+ULTRAPAIN@users.noreply.github.com>
2024-08-29 18:51:02 +0530

    Added the user dashboard page
    Co-authored-by: Akhilesh Singh  and adityaVishwakarma

---
+++ src/pages/user_dashboard.tsx
@@ -0,0 +1,9 @@
+import UserLayout from "./user_layout"
+export default function Dashboard(){
+  return <>
+  <div className="p-5">
+    <h1 className="pl-20 text-4xl font-black">Dashboard</h1>
+  </div>
+  <UserLayout />
+  </>
+}
