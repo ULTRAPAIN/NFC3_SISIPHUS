6772c1c4ead3b300c4b0c7b98a6da03c71231ae5
Dashrath Patel <108078023+ULTRAPAIN@users.noreply.github.com>
2024-08-29 18:47:44 +0530

    Changes done in the app Component
    Co-authored-by: Akhilesh Singh  and adityaVishwakarma

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,28 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from "@/components/ui/button"
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserHome from './pages/user_homepage';
+import Dashboard from './pages/user_dashboard';
+import UserOrganisation from './pages/user_organisation';
+import UserTransactions from './pages/user_transactions';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-          <Button>Click me</Button>
-    </>
+    <div>
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<UserHome />}/>
+        <Route  path='user/dashboard' element={<Dashboard />}/>
+        <Route path='user/organisation' element={<UserOrganisation />}/>
+        <Route path='user/transactions' element={<UserTransactions />}/>
+      </Routes>
+      
+    </BrowserRouter>
+    </div>
   )
 }
 
